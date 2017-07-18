5a315357b4303ca9cee17c35373f830ded849641
ArionasMC <nick.mccrafter@gmail.com>
2017-07-18 21:30:32 +0300

    Add files via upload

---
+++ src/main/java/me/amc/mybiomes/MyBiomes.java
@@ -0,0 +1,48 @@
+package me.amc.mybiomes;
+
+import com.mojang.realmsclient.dto.RealmsServer.WorldType;
+
+import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraftforge.common.BiomeManager;
+import net.minecraftforge.common.BiomeManager.BiomeEntry;
+import net.minecraftforge.common.BiomeManager.BiomeType;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+@Mod(modid = MyBiomes.MODID, version = MyBiomes.VERSION)
+public class MyBiomes {
+	
+    public static final String MODID = "examplemod";
+    public static final String VERSION = "1.0";
+    
+    public static BiomeGenBase diamondWoods = new BiomeGenWoods(52).setBiomeName("DiamondWoods");
+    
+    ExampleHandler exampleHandler = new ExampleHandler();
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+    	
+    //	BiomeManager.addSpawnBiome(diamondWoods);
+    //	BiomeManager.addBiome(BiomeType.COOL, new BiomeEntry(diamondWoods, 10));
+    	
+    //	WorldChunkManager.allowedBiomes.add(diamondWoods);
+    	
+    //	BiomeDictionary.registerBiomeType(diamondWoods, Type.FOREST);
+    //	BiomeManager.addSpawnBiome(diamondWoods);
+    	BiomeManager.addBiome(BiomeType.COOL, new BiomeEntry(diamondWoods, 1));
+    	
+    //	GameRegistry.registerWorldGenerator(exampleHandler, 0);
+    }
+    
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+		
+    //	BiomeManager.addSpawnBiome(diamondWoods);
+    //	BiomeManager.addBiome(BiomeType.COOL, new BiomeEntry(diamondWoods, 50));
+    	
+    }
+}
